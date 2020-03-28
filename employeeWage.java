c730132bf01688d31717a2b3e549f70097e953e1
sagar-528 <gupta.sagar528@gmail.com>
2020-03-28 23:43:14 +0530

    ADDED: Calculation

--- employeeWage.java
+++ employeeWage.java
@@ -3,6 +3,9 @@ public class employeeWage
 	public static void main(String[] args) {
 		//Constatnt
 		int IS_FULL_TIME = 1;
+		int EMP_WAGE_PER_HRS = 20;
+		int FULL_DAY_HRS = 8;
+
 		//Random function
 		double check = Math.floor(Math.random() * 10) % 2;
 
@@ -12,5 +15,8 @@ public class employeeWage
 		else
 			System.out.println("Employee is absent");
 		
+		//Calculating daily wages
+		int empWage = EMP_WAGE_PER_HRS * FULL_DAY_HRS;
+		System.out.println("The daily Employee wage is= "+empWage);
 	}
 }
