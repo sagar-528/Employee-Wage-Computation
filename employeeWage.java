1876e3cf425514a56a8551bc4674864559e5146b
sagar-528 <gupta.sagar528@gmail.com>
2020-03-29 01:18:21 +0530

    ADDED: Days

--- employeeWage.java
+++ employeeWage.java
@@ -5,16 +5,18 @@ public class employeeWage
 		public static final int IS_PART_TIME = 1;
 		public static final int PART_TIME_HRS= 8;
 		public static final int NUM_WORKING_DAYS = 2;
+		public static final int MAX_HRS_MONTH = 10;
 
 	public static void main(String[] args) {
 
 		//Variables
 		int empHrs = 0;
-		int totalWage = 0;
-
+		int totalEmpHrs = 0;
+		int totalWorkingDays= 0;
 		//Computation
-		for (int Counter = 0; Counter < NUM_WORKING_DAYS; Counter++){
+		while (totalEmpHrs <= MAX_HRS_MONTH && totalWorkingDays < NUM_WORKING_DAYS){
 		
+		totalWorkingDays++;
 		int check = (int) Math.floor(Math.random() * 10) % 2;
 
 			//Condition using Switch case.
@@ -30,10 +32,10 @@ public class employeeWage
 			}
 
 			//Calculating daily wages
-			int empWage = EMP_WAGE_PER_HRS * empHrs;
-			totalWage += empWage;
-			System.out.println("The daily Employee wage is= "+empWage);
+			totalEmpHrs += empHrs;
+			System.out.println("Day#: " + totalWorkingDays + "empHr: " +empHrs);
 		}
-		System.out.println("Total Employee Wage is= "+totalWage);
+		int totalEmpWage = totalEmpHrs * EMP_WAGE_PER_HRS;
+		System.out.println("Total Employee Wage is= "+totalEmpWage);
 	}
 }
