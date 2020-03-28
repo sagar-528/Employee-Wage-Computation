0853ec98e46c660690f7fb0e018ea777aba27a4d
sagar-528 <gupta.sagar528@gmail.com>
2020-03-29 00:11:36 +0530

    ADDED: Part_Time

--- employeeWage.java
+++ employeeWage.java
@@ -4,19 +4,24 @@ public class employeeWage
 		//Constatnt
 		int IS_FULL_TIME = 1;
 		int EMP_WAGE_PER_HRS = 20;
-		int FULL_DAY_HRS = 8;
+		int IS_PART_TIME = 2;
+
+		//Variables
+		int empHrs = 0;
 
 		//Random function
 		double check = Math.floor(Math.random() * 10) % 2;
 
 		//Condition for checking present or absent
 		if ( check == IS_FULL_TIME ) 
-			System.out.println("Employee is present");
+			empHrs = 8;
+		else if ( check == IS_PART_TIME )
+			empHrs = 8;
 		else
-			System.out.println("Employee is absent");
+			empHrs=0;
 		
 		//Calculating daily wages
-		int empWage = EMP_WAGE_PER_HRS * FULL_DAY_HRS;
+		int empWage = EMP_WAGE_PER_HRS * empHrs;
 		System.out.println("The daily Employee wage is= "+empWage);
 	}
 }
