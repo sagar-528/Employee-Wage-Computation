02c3961142a93ec1b9a6190367755402f618304a
sagar-528 <gupta.sagar528@gmail.com>
2020-03-29 00:28:54 +0530

    ADDED: Using Switch Cases

--- employeeWage.java
+++ employeeWage.java
@@ -1,25 +1,29 @@
 public class employeeWage
 {
+	//Constatnt
+		public static final int EMP_WAGE_PER_HRS = 20;
+		public static final int IS_PART_TIME = 1;
+
 	public static void main(String[] args) {
-		//Constatnt
-		int IS_FULL_TIME = 1;
-		int EMP_WAGE_PER_HRS = 20;
-		int IS_PART_TIME = 2;
 
 		//Variables
 		int empHrs = 0;
 
 		//Random function
-		double check = Math.floor(Math.random() * 10) % 2;
+		int check = (int) Math.floor(Math.random() * 10) % 2;
 
-		//Condition for checking present or absent
-		if ( check == IS_FULL_TIME ) 
+		//Condition using Switch case.
+		switch (check) {
+
+		case IS_PART_TIME :
 			empHrs = 8;
-		else if ( check == IS_PART_TIME )
-			empHrs = 8;
-		else
+			break;
+
+		default :
 			empHrs=0;
 		
+		}
+
 		//Calculating daily wages
 		int empWage = EMP_WAGE_PER_HRS * empHrs;
 		System.out.println("The daily Employee wage is= "+empWage);
